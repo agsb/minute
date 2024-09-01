f5ef9ac0605d03c5cda35fe3f00e4bc33182c426
agsb <barcellos.alvaro@gmail.com>
2024-09-01 19:19:33 -0300

    update kernel and java counts

--- docs/java.sources.md
+++
@@ -1,8 +0,0 @@
-
-## Frequency for JAVA
-
-[TheAlgorithms/Java](git@github.com:TheAlgorithms/Java.git)
-
-[Open JDK](git@github.com:openjdk/jdk.git)
-
-
--- srcs/preper.sh
+++
@@ -1,4 +0,0 @@
-find ../Gits/linux-6.10.7/  -name '*.c' -exec cat \{\} \; > allc.lst
-
-find ../Gits/linux-6.10.7/  -name '*.h' -exec cat \{\} \; > allh.lst
-
