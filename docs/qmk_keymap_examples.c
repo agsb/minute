11a93c2494499feb7cb29867266d478d98e5b6c1
Alvaro Gomes Sobral Barcellos <barcellos.alvaro@gmail.com>
2024-09-11 14:41:39 -0300

    Update qmk_keymap_examples.c

--- docs/qmk_keymap_examples.c
+++ docs/qmk_keymap_examples.c
@@ -1,4 +1,62 @@
+/*
 
+**config.h**
+```
+#define TAPPING_TERM 200
+#define DYNAMIC_TAPPING_TERM_INCREMENT 5
+#define TAPPING_TERM_PER_KEY
+
+// If QUICK_TAP_TERM is set to 0, the second press will always be sent, effectively disabling auto-repeat.
+// use eg. SFT_T(KC_A, LSFT)
+#define QUICK_TAP_TERM 120
+
+// try
+#define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD_PER_KEY
+// or 
+#define HOLD_ON_OTHER_KEY_PRESS
+// no other key pressed results in nothing happening, just the normal keycode
+// LT(2, KC_SPC) results in KC_SPC
+#define RETRO_TAPPING
+#define RETRO_TAPPING_PER_KEY
+#define DUMMY_MOD_NEUTRALIZER_KEYCODE KC_ANY
+#define MODS_TO_NEUTRALIZE { <mod_mask_1>, <mod_mask_2>, ... }
+// Neutralize left alt and left GUI (Default value)
+// #define MODS_TO_NEUTRALIZE { MOD_BIT(KC_LEFT_ALT), MOD_BIT(KC_LEFT_GUI) }
+
+// like retro_tapping but special for shift
+#define RETRO_SHIFT
+
+// also {set, get}_tri_layer_{lower,upper,adjust}_layer()
+#define TRI_LAYER_LOWER_LAYER 8
+#define TRI_LAYER_UPPER_LAYER 2
+#define TRI_LAYER_ADJUST_LAYER 3
+
+```
+---
+**rules.mk**
+
+```
+
+DYNAMIC_TAPPING_TERM_ENABLE
+QK_DYNAMIC_TAPPING_TERM_PRINT, DT_PRNT, Types the current tapping term, in milliseconds
+QK_DYNAMIC_TAPPING_TERM_UP, DT_UP, Increases the current tapping term by DYNAMIC_TAPPING_TERM_INCREMENTms (5ms by default)
+QK_DYNAMIC_TAPPING_TERM_DOWN, DT_DOWN, Decreases the current tapping term by DYNAMIC_TAPPING_TERM_INCREMENTms (5ms by default)
+
+// enables 4 layers,   actual (none), lower TL_LOWR , upper TL_UPPR, adjust (both)
+TRI_LAYER_ENABLE = yes
+
+// for UTF_8
+UNICODE_COMMON = yes
+
+// define source, minute is a stub
+SRC += minute.c
+```
+┌────┬────┬────┬────┐                   ┌────┬────┬────┬────┐
+├────┼────┼────┼────┤                   ├────┼────┼────┼────┤
+├────┼────┼────┼────╱────╱         ╲────╲────┼────┼────┼────┤
+                        ╱────╱ ╲────╲
+*/
 
 // from <https://thomasbaart.nl/2018/12/09/qmk-basics-tap-and-hold-actions/>
 
